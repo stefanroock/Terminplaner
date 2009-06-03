23b7a7bb3ee098a451330408c4624ef371e38918
Stefan Roock <stefan@stefanroock.de>
2009-06-03 21:30:23 +0200

    Auswertung

--- Terminplaner-Lisp/Teilnahme.lisp
+++
@@ -1,8 +0,0 @@
-(defclass Teilnahme () ((teilnehmer :accessor teilnehmer :initarg :teilnehmer) 
-                        (status :accessor status :initarg :status :initform 'offen)))
-
-(defmethod bestaetige ((tn Teilnahme)) (setf (status tn) 'bestaetigt))
-
-(defmethod lehne-ab ((tn Teilnahme)) (setf (status tn) 'abgelehnt))
-
-(defmethod ist-abgelehnt ((tn Teilnahme)) (eql (status tn) 'abgelehnt))
